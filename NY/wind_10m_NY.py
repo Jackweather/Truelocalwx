4c228c708c17d2c42f326d05fb9446f0d3d8c861
Jackweather <159730073+Jackweather@users.noreply.github.com>
2026-02-20 20:12:02 -0500

    Update wind_10m_NY.py

--- NY/wind_10m_NY.py
+++ NY/wind_10m_NY.py
@@ -256,7 +256,7 @@ def plot_wind_10m(u_path, v_path, step):
         ]
     )
     ax.text(
-        text_x, text_y_base, "Truelocalwx.com",
+        text_x, text_y_base, "NYWeatherModels.com",
         fontsize=7, color="black", ha="right", va="bottom",
         fontweight="normal", alpha=0.85,
         transform=ccrs.PlateCarree(),
